018fe3ad479cb45d739ce30ae58bcf57e4d48542
m.horii <m.horii@kyoeisystem.co.jp>
2024-10-10 11:02:07 +0900

    setup

---
+++ Horii_HHT/app/src/main/java/com/example/horii_hht/StartScreen.kt
@@ -0,0 +1,21 @@
+package com.example.horii_hht
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // activity_main を表示
+
+        // 数秒後に MainActivity に遷移
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000ミリ秒（3秒）
+    }
+}
